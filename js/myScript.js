fdf7817cca3b24290cf551b526df57fd51fb9a9d
Eric Rion <erion42@gmail.com>
2021-09-24 20:43:21 -0400

    Initial commit. Completely forgot about this. Lots of updates.

---
+++ js/myScript.js
@@ -0,0 +1,10 @@
+// Time Test
+var today = moment().format("ddd MMM Do, YYYY");
+$("#currentDay").text(today);
+
+var thisHour = moment().format("hh");
+console.log(thisHour);
+
+for (var i=1; i < 25;i++) {
+    console.log(i);
+}
