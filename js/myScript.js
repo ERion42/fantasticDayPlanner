b8602618723a4bdee7640b5fe2423f58a8f64ff2
Eric Rion <erion42@gmail.com>
2021-09-24 22:12:39 -0400

    Most of the HTML and CSS is done. Need to work on saving data next.

--- js/myScript.js
+++ js/myScript.js
@@ -2,9 +2,34 @@
 var today = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text(today);
 
-var thisHour = moment().format("hh");
-console.log(thisHour);
+// Change color class based on time now vs time on the scheduler
+$(".row").children("div.timeBlock").each(function() {
+    var divTime = moment($(this).text().trim(), 'h A');
+    var nowTime = moment().format();
+    if (divTime.isSame(nowTime, 'hour')) {
+        $(this).next().addClass('present');
+    }
 
-for (var i=1; i < 25;i++) {
-    console.log(i);
+    if (divTime.isAfter(nowTime, 'hour')) {
+        $(this).next().addClass('future');
+    }
+
+    if (divTime.isBefore(nowTime, 'hour')) {
+        $(this).next().addClass('past');
+    }
+
+    console.log(divTime);
+    console.log(nowTime);
+    console.log(divTime.isSame(nowTime, 'hour'));
+    console.log(divTime.isAfter(nowTime, 'hour'));
+    console.log(divTime.isBefore(nowTime, 'hour'));
+    
+})
+
+
+// Save button
+// on click, localstorage.setitem("")
+// each text area should have its own key
+function buttonClick() {
+    localStorage.setItem("")
 }
